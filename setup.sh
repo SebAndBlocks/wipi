e445c0a8229e373a10f66469b06cbd69c4f237ee
Seb <69688534+SebTNT@users.noreply.github.com>
2023-06-27 16:49:46 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 echo "Welcome to WiPi by TurquoiseTNT"
 echo "Setup will now Start"
+sudo apt-get install wget
 echo "-------------------------------"
 echo "Step 1: Installing RaspAP Web GUI"
 sudo raspi-config
@@ -77,18 +78,8 @@ echo "addn-hosts=/etc/raspap/adblock/hostnames.txt" | sudo tee -a /etc/dnsmasq.d
 sudo sed -i '/dhcp-option=6/d' /etc/dnsmasq.d/090_raspap.conf
 sudo sed -i "s/\('RASPI_ADBLOCK_ENABLED', \)false/\1true/g" includes/config.php
 echo "Step 2: Editing SSID and Password Settings for your RaspAP."
-# Specify the new WiFi broadcast settings
-rs=$(cat /dev/urandom | tr -dc 'A-Z0-9' | fold -w 4 | awk '{print substr($0,1,1) substr($0,2,1) substr($0,3,1) substr($0,4,1)}')
-ssid="MyWiPi_$rs"
-wpa_passphrase="wipi-$rs"
-# Use sed to modify the configuration file
-sudo sed -i "s/^ssid=.*/ssid=$ssid/" /etc/hostapd/hostapd.conf
-sudo sed -i "s/^wpa_passphrase=.*/wpa_passphrase=$wpa_passphrase/" /etc/hostapd/hostapd.conf
-# Change the router IP address to 192.168.10.1
-sudo sed -i 's/^interface=wlan0/#&\ninterface=wlan0\n\ndhcp-range=192.168.10.2,192.168.10.254,255.255.255.0,24h\n\naddress=/double-click.net/192.168.10.1/' /etc/dnsmasq.d/090_raspap.conf
-# Allow access to devices on the main router at 192.168.0.*
-sudo sed -i 's/^#dhcp-option=3.*/dhcp-option=3,192.168.10.1/' /etc/dnsmasq.d/090_raspap.conf
-sudo sed -i 's/^#dhcp-option=6.*/dhcp-option=6,192.168.0.1/' /etc/dnsmasq.d/090_raspap.conf
+sudo wget https://raw.githubusercontent.com/SebTNT/wipi/main/boot1-settings.sh /usr/wipi/boot.sh
+sudo sed -i 's/^exit 0$/bash \/usr\/wipi\/boot.sh\nexit 0/' /etc/rc.local
 echo "Step 3: Installing WiPi Services (eg: samba, coder )"
 sudo apt install samba -y
 sudo tee /etc/samba/smb.conf > /dev/null <<EOF
